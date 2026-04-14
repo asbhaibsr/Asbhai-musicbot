0b0d215a15e1b3074ea3fe8525297d8afa9fc8b4
Asbhaibsr <ffloverisarsad@gmail.com>
2026-04-14 08:06:39 +0530

    Update video.py

--- AsbhaiMusic/plugins/tools/video.py
+++ AsbhaiMusic/plugins/tools/video.py
@@ -98,5 +98,5 @@ async def download_video(_, message: Message):
         await m.delete()
 
     except Exception as e:
-        await m.edit("🙂 ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ! @itsMeShivanshu & @ShivanshuXD ᴘᴍ ")
+        await m.edit("🙂 ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ! @asbhaibsr ᴘᴍ ")
         print(f"Error: {str(e)}")
